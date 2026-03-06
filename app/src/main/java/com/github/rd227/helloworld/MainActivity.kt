c006e2f91173495618a4d362cee4f73a5b3da96d
rD227 <2329976818@qq.com>
2026-03-06 16:47:03 +0800

    Check app/src/main/java/com/github/rd227/helloworld/ui/theme/Theme.kt

--- app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
+++ app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.github.rd227.helloworld
 
+import android.R.id.message
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 //import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,8 +20,56 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.rd227.helloworld.ui.theme.WelcomeApplicationTheme
 
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WelcomeApplicationTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GreetingText(message: String,modifier: Modifier = Modifier,from: String) {
+    Surface(color = Color.Cyan) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            modifier = modifier.padding(8.dp),
+            lineHeight = 120.sp,
+        )
+    }
+    Text(
+        text = from,
+        fontSize = 36.sp,
+        modifier = modifier
+            .padding(1.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    WelcomeApplicationTheme() {
+        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+    }
+}
+
+
+/*
+*
+*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,3 +105,6 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+*
+*
+*/
