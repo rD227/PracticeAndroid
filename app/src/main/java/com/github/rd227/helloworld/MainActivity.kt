0a5c2522b9d85fcbd6938536064f55832271879f
rD227 <2329976818@qq.com>
2026-03-06 11:55:00 +0800

    Initial background

--- app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
+++ app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.rd227.helloworld.ui.theme.WelcomeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,11 +35,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+//@Preview(showBackground = true)
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello First $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Gray) {
+        Text(
+            text = "Hello First $name!",
+            modifier = modifier.padding(80.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
