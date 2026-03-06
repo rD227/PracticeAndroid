20b55db386787245be27e04c4d25bf48d017be63
rD227 <2329976818@qq.com>
2026-03-06 17:22:10 +0800

    Part

--- app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
+++ app/src/main/java/com/github/rd227/helloworld/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 //import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,8 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,20 +46,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String,modifier: Modifier = Modifier,from: String) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            modifier = modifier.padding(8.dp),
-            lineHeight = 120.sp,
-        )
-    }
-    Text(
-        text = from,
-        fontSize = 36.sp,
-        modifier = modifier
-            .padding(1.dp)
-    )
+    //Row {
+        Surface(color = Color.Cyan) {
+            Column {
+                Text(
+                    text = message,
+                    fontSize = 105.sp,
+                    modifier = modifier.padding(8.dp),
+                    lineHeight = 120.sp,
+                    TextAlign = TextAlign.Center
+                )
+                Text(
+                    text = from,
+                    fontSize = 36.sp,
+                    modifier = modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                )
+            }
+        }
+    //}
 }
 
 @Preview(showBackground = true)
