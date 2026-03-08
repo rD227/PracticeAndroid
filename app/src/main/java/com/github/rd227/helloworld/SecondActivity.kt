dea6cac3c76d6743b3c35be4c6edadce10e138bf
rD227 <2329976818@qq.com>
2026-03-08 22:39:58 +0800

    Second Activiy

--- app/src/main/java/com/github/rd227/helloworld/SecondActivity.kt
+++ app/src/main/java/com/github/rd227/helloworld/SecondActivity.kt
@@ -3,14 +3,37 @@ package com.github.rd227.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.rd227.helloworld.ui.theme.WelcomeApplicationTheme
 
-class SecondActivity : ComponentActivity(){
-   override fun oncreate(savedInstanceState: Bundle?){
-        super.oncreate(savedInstanceState)
-        setContent{
-            Text(text = "Second Activity")
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WelcomeApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorScheme.background
+                ) {
+                    Text(text = "Second Activity")
+                    SecondActivity()
+                }
+            }
         }
     }
 }
 
+@Composable
+@Preview(showBackground = true)
+fun SecondActivityPreview() {
+    WelcomeApplicationTheme {
+        Text(text = "Second Activity")
+    }
+}
