6c34f4c60cb59cfc081ca54dbf79cc20f1b21eb0
rD227 <2329976818@qq.com>
2026-03-11 22:19:58 +0800

    Font

--- app/src/main/java/com/github/rd227/helloworld/ui/theme/Type.kt
+++ app/src/main/java/com/github/rd227/helloworld/ui/theme/Type.kt
@@ -2,11 +2,17 @@ package com.github.rd227.helloworld.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.github.rd227.helloworld.R
 
 // Set of Material typography styles to start with
+
+val RoundFont = FontFamily(
+    Font(R.font.alimama_fang_yuan, FontWeight.Normal)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
