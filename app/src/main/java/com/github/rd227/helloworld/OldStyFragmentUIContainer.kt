685520fe7c11791a34e7734d9c7cb5af361e92e6
rD227 <2329976818@qq.com>
2026-03-30 23:03:05 +0800

    OldFragmentUI test

--- app/src/main/java/com/github/rd227/helloworld/OldStyFragmentUIContainer.kt
+++ app/src/main/java/com/github/rd227/helloworld/OldStyFragmentUIContainer.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 class OldStyFragmentUIContainer : Fragment() {
@@ -14,3 +19,16 @@ class OldStyFragmentUIContainer : Fragment() {
         return inflater.inflate(R.layout.preferences, container, false)
     }
 }
+
+@Preview
+@Composable
+fun OldStyFragmentUIPreview() {
+    AndroidView(
+        factory = { context ->
+            LayoutInflater.from(context).inflate(R.layout.preferences, null)
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+
